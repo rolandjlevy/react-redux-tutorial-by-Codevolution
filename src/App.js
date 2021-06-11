44f2d0dd76c55a1f94764ea8b9b612301f574cea
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-06-11 15:19:16 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';        
+import CakeContainer from './components/CakeContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <CakeContainer />
+    </div>
+  );
+}
+
+export default App;
